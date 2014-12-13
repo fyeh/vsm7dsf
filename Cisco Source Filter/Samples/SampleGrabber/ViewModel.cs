93d6e3074a3261b4dca14195126ecb346598cd2e
fyeh@us.ibm.com <fyeh@us.ibm.com>
2014-12-13 14:51:18 -0800

    removed invalid copyright comments
    Removed comments about copyrights that were accidently copied into files

--- Cisco Source Filter/Samples/SampleGrabber/ViewModel.cs
+++ Cisco Source Filter/Samples/SampleGrabber/ViewModel.cs
@@ -1,18 +1,4 @@
-﻿// IBM Confidential
-//
-// OCO Source Material
-//
-// 5725H94
-//
-// (C) Copyright IBM Corp. 2005,2006
-//
-// The source code for this program is not published or otherwise divested
-// of its trade secrets, irrespective of what has been deposited with the
-// U. S. Copyright Office.
-//
-// US Government Users Restricted Rights - Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with
-// IBM Corp.
+﻿
 
 using System;
 using System.Collections.ObjectModel;
